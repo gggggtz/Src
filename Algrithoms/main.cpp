e7df89ef28548d041c3b272e6b527ba9415cc150
Charlie Gu <gggggtz@hotmail.com>
2014-07-02 13:47:23 -0400

    Initial Setup

---
+++ Algrithoms/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: root
+ *
+ * Created on July 2, 2014, 1:46 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    return 0;
+}
+
