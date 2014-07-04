b66af08bae48673bacacfaffc4dbc02a231d5e94
Charlie Gu <gggggtz@hotmail.com>
2014-07-04 16:07:45 +0800

    Add RpcInterface

---
+++ VsCpp/RpcInterface/SampleRpc_h.h
@@ -0,0 +1,76 @@
+
+
+/* this ALWAYS GENERATED file contains the definitions for the interfaces */
+
+
+ /* File created by MIDL compiler version 8.00.0603 */
+/* at Wed Jul 02 11:26:25 2014
+ */
+/* Compiler settings for SampleRpc.idl:
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
+    protocol : dce , ms_ext, app_config, c_ext, robust
+    error checks: allocation ref bounds_check enum stub_data 
+    VC __declspec() decoration level: 
+         __declspec(uuid()), __declspec(selectany), __declspec(novtable)
+         DECLSPEC_UUID(), MIDL_INTERFACE()
+*/
+/* @@MIDL_FILE_HEADING(  ) */
+
+#pragma warning( disable: 4049 )  /* more than 64k source lines */
+
+
+/* verify that the <rpcndr.h> version is high enough to compile this file*/
+#ifndef __REQUIRED_RPCNDR_H_VERSION__
+#define __REQUIRED_RPCNDR_H_VERSION__ 475
+#endif
+
+#include "rpc.h"
+#include "rpcndr.h"
+
+#ifndef __RPCNDR_H_VERSION__
+#error this stub requires an updated version of <rpcndr.h>
+#endif // __RPCNDR_H_VERSION__
+
+
+#ifndef __SampleRpc_h_h__
+#define __SampleRpc_h_h__
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+/* Forward Declarations */ 
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+
+#ifndef __SampleRpc_INTERFACE_DEFINED__
+#define __SampleRpc_INTERFACE_DEFINED__
+
+/* interface SampleRpc */
+/* [implicit_handle][version][uuid] */ 
+
+void Output( 
+    /* [string][in] */ const char *szOutput);
+
+
+extern handle_t hSampleRpcBinding;
+
+
+extern RPC_IF_HANDLE SampleRpc_v1_0_c_ifspec;
+extern RPC_IF_HANDLE SampleRpc_v1_0_s_ifspec;
+#endif /* __SampleRpc_INTERFACE_DEFINED__ */
+
+/* Additional Prototypes for ALL interfaces */
+
+/* end of Additional Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
