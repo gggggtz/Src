c6a96e65f927c52a00d0fd4edc1785b845f23987
CharlieGu <gggggtz@hotmail.com>
2014-08-05 22:22:04 +0800

    Add eventlog.ps1

---
+++ Powershell/EventLog.ps1
@@ -0,0 +1,11 @@
+﻿Limit-EventLog -LogName Application -MaximumSize 1024 -OverflowAction DoNotOverwrite
+$charlieAppLog = Get-EventLog -LogName Application -Source Charlie -Newest 1 -ErrorAction SilentlyContinue
+if(!$charlieAppLog)
+{   
+    New-EventLog -LogName Application -Source Charlie
+}
+
+for($i = 1; $i -le 100000; $i++)
+{
+	Write-EventLog -LogName Application -Source Charlie -EntryType Information -Message "Test Message $i" -EventId $i -Category 1
+}
