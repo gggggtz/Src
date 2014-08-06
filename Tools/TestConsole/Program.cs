0c99b92a14e6b35059fbbf78cabfc491a4f9241a
CharlieGu <gggggtz@hotmail.com>
2014-08-06 21:09:36 +0800

    Add a Test project

---
+++ Tools/TestConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = System.Text.Encoding.Default.GetBytes("I am not a robot");//以字节方式存储
+            System.Security.Cryptography.SHA1 sha1 = new System.Security.Cryptography.SHA1CryptoServiceProvider();
+            byte[] result = sha1.ComputeHash(data);//得到哈希值
+            string r1 = System.BitConverter.ToString(result).Replace("-", ""); //转换成为字符串的显示
+            Console.WriteLine(r1);
+            string r2 = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile("I am not a robot", "SHA1");
+            Console.WriteLine(r2);
+            Console.ReadLine();
+        }
+    }
+}
